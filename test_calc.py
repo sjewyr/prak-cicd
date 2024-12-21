e720ea23832dfc0c5c57083aba32da71e0ba98cf
HideAndSeekMan <manofironboss@gmail.com>
2024-12-21 09:38:04 +0300

    fix/test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -5,28 +5,32 @@ from PyQt6.QtCore import Qt
 
 def test_plus(qtbot:pytestqt.qtbot.QtBot):
     window = CalculatorWindow()
-    qtbot.keyPress(window, '1')
+    qtbot.keyPress(window, '6')
     qtbot.keyPress(window, '+')
-    qtbot.keyPress(window, '1')
+    qtbot.keyPress(window, '3')
+    qtbot.mouseClick(window.invert_button, Qt.MouseButton.LeftButton)
     qtbot.mouseClick(window.equals_button, Qt.MouseButton.LeftButton)
-    assert window.monitor.text() == "2.0"
+    assert window.monitor.text() == "3.0"
 
 
 def test_minus(qtbot:pytestqt.qtbot.QtBot):
     window = CalculatorWindow()
-    qtbot.keyPress(window, '1')
-    qtbot.keyPress(window, '-')
-    qtbot.keyPress(window, '1')
+    qtbot.keyPress(window, '9')
+    qtbot.mouseClick(window.invert_button, Qt.MouseButton.LeftButton)
+    qtbot.mouseClick(window.sqrt_button, Qt.MouseButton.LeftButton)
     qtbot.mouseClick(window.equals_button, Qt.MouseButton.LeftButton)
-    assert window.monitor.text() == "0.0"
+    assert window.monitor.text() == "Error"
 
 def test_mult(qtbot:pytestqt.qtbot.QtBot):
     window = CalculatorWindow()
     qtbot.keyPress(window, '2')
+    qtbot.mouseClick(window.point_button, Qt.MouseButton.LeftButton)
+    qtbot.keyPress(window, '8')
+    qtbot.mouseClick(window.invert_button, Qt.MouseButton.LeftButton)
     qtbot.keyPress(window, '*')
     qtbot.keyPress(window, '5')
     qtbot.mouseClick(window.equals_button, Qt.MouseButton.LeftButton)
-    assert window.monitor.text() == "10.0"
+    assert window.monitor.text() == "-14.0"
 
 def test_div(qtbot:pytestqt.qtbot.QtBot):
     window = CalculatorWindow()
@@ -35,6 +39,16 @@ def test_div(qtbot:pytestqt.qtbot.QtBot):
     qtbot.keyPress(window, '4')
     qtbot.mouseClick(window.equals_button, Qt.MouseButton.LeftButton)
     assert window.monitor.text() == "2.25"
+
+def test_trygonometr(qtbot:pytestqt.qtbot.QtBot):
+    window = CalculatorWindow()
+    qtbot.keyPress(window, '3')
+    qtbot.mouseClick(window.point_button, Qt.MouseButton.LeftButton)
+    qtbot.keyPress(window, '1')
+    qtbot.keyPress(window, '4')
+    qtbot.mouseClick(window.cos_button, Qt.MouseButton.LeftButton)
+    qtbot.mouseClick(window.equals_button, Qt.MouseButton.LeftButton)
+    assert round(float(window.monitor.text()),1) == -1.0
 
 def test_mem(qtbot:pytestqt.qtbot.QtBot):
     window = CalculatorWindow()
